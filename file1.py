2b6182461a612ff6fe620cc90fceaeef39c9b97a
kapil <kapilkale@gmail.com>
2020-09-04 21:09:58 +0530

    version9

--- file1.py
+++ file1.py
@@ -1,1 +1,29 @@
-print('test program')
+x = 10
+print(x)
+x = 20
+print(x)
+print(type(x))
+print(id(x))
+''' the type function is taking the variable name as input and returning the data type of the variable as output'''
+x = 'kapil'
+print(x)
+print(type(x))
+print(id(x))
+x = 'kushagr'
+print(x)
+print(id(x))
+
+a = 10
+print(type(a))
+''' <class 'int'> '''
+type(a)
+''' does not show any output '''
+print(a)
+
+b = 1234567891111111111111111111110983408204890238902890940294029340
+print(b)
+
+total = 10
+print(total)
+TOTAL = 999
+print(TOTAL)
